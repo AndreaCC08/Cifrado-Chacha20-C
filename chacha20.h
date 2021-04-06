74e6426605931c9dadb81969649f38b7694601dc
Andrea Calero Caro <alu0101202952@ull.edu.es>
2021-04-06 16:48:20 +0100

    Cifrado Chacha20 c, comentarios hechos final

--- chacha20.h
+++ chacha20.h
@@ -6,13 +6,10 @@
 * Universidad de La Laguna 
 ********************************************************************/
 
-#ifndef __CHACHA20_H
-#define __CHACHA20_H
+#pragma once
 #include <stdint.h>
 
 /*
  * Función ChaCha20XOR que encripta y desencripta el mensaje
  */
 void ChaCha20XOR(uint8_t key[32], uint32_t counter, uint8_t nonce[12], uint8_t *input, uint8_t *output, int inputlen);
-
-#endif
